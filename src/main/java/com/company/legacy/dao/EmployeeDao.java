3cf736ed27e443a342c99948fabebba4d8ada050
Jon Iriarte Sulleiro <jon.iriarte-sulleiro@capgemini.com>
2026-05-06 14:04:09 +0200

    Full CRUD working

--- src/main/java/com/company/legacy/dao/EmployeeDao.java
+++ src/main/java/com/company/legacy/dao/EmployeeDao.java
@@ -3,6 +3,7 @@ package com.company.legacy.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,15 +11,14 @@ import com.company.legacy.model.Employee;
 import com.company.legacy.util.DBConnection;
 
 public class EmployeeDao {
- public List<Employee> findAll() throws Exception {
+    public List<Employee> findAll() throws Exception {
 
-    
-List<Employee> employees = new ArrayList<>();
+        List<Employee> employees = new ArrayList<>();
 
         String sql = "SELECT * FROM employee";
         try (Connection conn = DBConnection.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql);
-             ResultSet rs = ps.executeQuery()) {
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()) {
 
             while (rs.next()) {
                 Employee e = new Employee();
@@ -26,14 +26,86 @@ List<Employee> employees = new ArrayList<>();
                 e.setFirstName(rs.getString("first_name"));
                 e.setLastName(rs.getString("last_name"));
                 e.setEmail(rs.getString("email"));
-            employees.add(e);
+                employees.add(e);
             }
         }
 
         return employees;
     }
 
+    public Employee findById(int id) {
+        String sql = "SELECT id, first_name, last_name, email FROM employee WHERE id = ?";
 
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
 
- }
+            ps.setInt(1, id);
 
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return mapRow(rs);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+    public void insert(Employee employee) {
+        String sql = "INSERT INTO employee (first_name, last_name, email) VALUES (?, ?, ?)";
+
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, employee.getFirstName());
+            ps.setString(2, employee.getLastName());
+            ps.setString(3, employee.getEmail());
+
+            ps.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(Employee employee) {
+        String sql = "UPDATE employee SET first_name = ?, last_name = ?, email = ? WHERE id = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, employee.getFirstName());
+            ps.setString(2, employee.getLastName());
+            ps.setString(3, employee.getEmail());
+            ps.setInt(4, employee.getId());
+
+            ps.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(int id) {
+        String sql = "DELETE FROM employee WHERE id = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Employee mapRow(ResultSet rs) throws SQLException {
+        Employee emp = new Employee();
+        emp.setId(rs.getInt("id"));
+        emp.setFirstName(rs.getString("first_name"));
+        emp.setLastName(rs.getString("last_name"));
+        emp.setEmail(rs.getString("email"));
+        return emp;
+    }
+
+}
--- src/main/java/com/company/legacy/soap/EmployeeService.java
+++ src/main/java/com/company/legacy/soap/EmployeeService.java
@@ -3,16 +3,15 @@ package com.company.legacy.soap;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
-
 
 import com.company.legacy.dao.EmployeeDao;
 import com.company.legacy.model.Employee;
 
-
-@WebService
+@WebService(name = "EmployeeService", targetNamespace = "http://legacy.company.com/soap", portName = "EmployeeServicePort")
 public class EmployeeService {
-    
+
     private EmployeeDao employeeDao = new EmployeeDao();
 
     @WebMethod
@@ -20,5 +19,24 @@ public class EmployeeService {
         return employeeDao.findAll();
     }
 
+    @WebMethod
+    public Employee getEmployeeById(@WebParam(name = "id") int id) {
+        return employeeDao.findById(id);
+    }
+
+    @WebMethod
+    public void addEmployee(Employee employee) {
+        employeeDao.insert(employee);
+    }
+
+    @WebMethod
+    public void updateEmployee(Employee employee) {
+        employeeDao.update(employee);
+    }
+
+    @WebMethod
+    public void deleteEmployee(@WebParam(name = "id") int id) {
+        employeeDao.delete(id);
+    }
 
 }
